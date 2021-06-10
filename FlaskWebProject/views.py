d9e25f5cd30e10b5ccfd2b6b76bcb9ef605876ab
AdityaGarg00 <61191738+AdityaGarg00@users.noreply.github.com>
2021-06-10 12:59:43 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -116,13 +116,17 @@ def _load_cache():
     return cache
 
 def _save_cache(cache):
-    # TODO: Save the cache, if it has changed
+    if cache.has_state_changed:
+        session['token_cache']=cache.serialized()
     pass
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
-    return None
+    return msla.ConfidentialClintApplication(
+        config.CLIENT_ID,authority=authority or Config.AUTHORITY,
+        client_credential=Config.CLIENT_SECRET,token_cache=cache)
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return None
+    return_build_msal_app(authority=authority).get_authorization_request_url(
+        scopes or [],
+        state=state or str(uuid.uuid4()),
+        redirect_uri=uri_for('authorized',_external=True,_scheme='https'))
